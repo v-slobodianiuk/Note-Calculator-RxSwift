37a8fd1051789b3e23a97fb3d6116d3bae0b8893
Vadym <dev.slobodianiuk@gmail.com>
2020-05-07 13:58:57 +0300

    Final logic adopted

--- Numbi/Views/NewNoteView.swift
+++ Numbi/Views/NewNoteView.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NewNoteView: UIView {
     
     var leftTextView = UITextView()
     var rightTextView = UITextView()
+    let keyboard = NumbiKeyboard()
+    var keyboardBar: UIToolbar!
+    private let disposeBag = DisposeBag()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -25,14 +30,66 @@ class NewNoteView: UIView {
         self.addSubview(leftTextView)
         self.addSubview(rightTextView)
         
-        leftTextView.backgroundColor = UIColor.systemBlue.withAlphaComponent(0.2)
-        rightTextView.backgroundColor = UIColor.systemBlue.withAlphaComponent(0.4)
+        //leftTextView.backgroundColor = UIColor.systemBlue.withAlphaComponent(0.2)
+        //rightTextView.backgroundColor = UIColor.darkGray.withAlphaComponent(0.9)
         
         leftTextView.font = .systemFont(ofSize: 18)
-        rightTextView.font = .systemFont(ofSize: 18)
+        rightTextView.font = .boldSystemFont(ofSize: 18)
+        rightTextView.isEditable = false
+        rightTextView.textAlignment = .right
         
-        //leftTextView.text = "1001010101010101110101110110101001110101010101100111010101010101100101"
-        //rightTextView.text = "1001010101010101110101110110101001110101010101100111010101010101100101"
+        keyboardBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 44))
+        let defaultKeyboard = UIBarButtonItem(title: "ABC", style: .plain, target: self, action: #selector(itemTapped))
+        let numbiKeyboard = UIBarButtonItem(title: "123", style: .plain, target: self, action: #selector(itemTapped))
+        keyboardBar.items = [defaultKeyboard, numbiKeyboard]
+        leftTextView.autocorrectionType = .no
+        leftTextView.inputAccessoryView = keyboardBar
+        leftTextView.inputView = keyboard
+        
+        keyboard.keyTitle
+            .subscribe(onNext: { keyValue in
+                self.keyWasTapped(character: keyValue)
+
+            })
+        .disposed(by: disposeBag)
+    }
+    
+    @objc func itemTapped(sender: UIBarButtonItem) {
+         guard let title = sender.title else { return }
+         switch title {
+         case "ABC":
+             leftTextView.inputView = nil
+             leftTextView.reloadInputViews()
+         default:
+             leftTextView.inputView = keyboard
+             leftTextView.reloadInputViews()
+         }
+     }
+    
+    func deleteWasTapped() {
+        leftTextView.deleteBackward()
+    }
+    
+    func keyWasTapped(character: String) {
+        
+        guard character != " " else { return }
+        
+        switch character {
+        case "+":
+            leftTextView.insertText("+")
+        case "−":
+            leftTextView.insertText("-")
+        case "×":
+            leftTextView.insertText("*")
+        case "÷":
+            leftTextView.insertText("/")
+        case "delete":
+            deleteWasTapped()
+        case "return":
+            leftTextView.insertText("\n")
+        default:
+            leftTextView.insertText(character)
+        }
     }
     
     func constraints() {
--- Numbi/Views/NewNoteViewController.swift
+++ Numbi/Views/NewNoteViewController.swift
@@ -14,11 +14,8 @@ import MathParser
 class NewNoteViewController: UIViewController {
     
     private let noteView = NewNoteView()
-    private let throttleIntervalInMilliseconds = 10
+    private let throttleIntervalInMilliseconds = 500
     private let disposeBag = DisposeBag()
-    
-    var textViewIndex = 0
-    var tempResult = ""
     
     override func loadView() {
         self.view = noteView
@@ -26,67 +23,113 @@ class NewNoteViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        left()
+        
+        title = "Numbi"
+        
+        noteView.leftTextView.becomeFirstResponder()
+        
+        setupInputView()
     }
     
-    func left() {
+    func setupInputView() {
         noteView.leftTextView.rx.text
             .observeOn(MainScheduler.asyncInstance)
             .distinctUntilChanged()
             .throttle(.milliseconds(self.throttleIntervalInMilliseconds), scheduler: MainScheduler.instance)
             .subscribe(onNext: {
-                //let rows = self.lineCounter(self.noteView.leftTextView)
-                //print(rows)
                 
+                var input = $0!.split(separator: "\n", omittingEmptySubsequences: false)
+                let result = input.transformOmittingEmptySubsequences("\n").map { String($0).math() }.joined(separator: "\n")
                 
+                self.noteView.rightTextView.text = result //String(format: "%.2f", result)
                 
-                guard let ch = $0?.last else { return }
-                
-                if ch == "\n" {
-                    let result = self.splitPlusSeparator($0!, separator: "\n")
-                }
-                
+                //self.atrString(str: result, textView: self.noteView.leftTextView)
             })
             .disposed(by: self.disposeBag)
     }
-
-    func splitPlusSeparator(_ string: String, separator: Character) -> [String] {
-        var result: Array<String> = []
-        var temp: Array<Character> = []
-        
-        string.forEach { c in
-            guard c == "!" else { return temp.append(c) }
-            result.append(temp.map({String($0)}).joined(separator: ""))
-            result.append("\(c)")
-            temp = []
-        }
-        result.append(temp.map({String($0)}).joined(separator: ""))
-        
-        return result
+    
+        func atrString(str: String?, textView: UITextView) {
+            let aString = self.customizeColor(string: "\(str ?? "")", color: UIColor.black)
+            
+    //        let chArray: Set<Character> = ["+", "-", "*", "/", "%"]
+    //
+    //        for c in chArray {
+    //            if (str?.contains("\(c)"))! {
+    //
+    //                let r1 = str!.range(of: "\(c)")!
+    //                let atr = self.customizeColor(string: "\(c)", color: UIColor.systemOrange)
+    //                aString.replaceCharacters(in: NSRange(r1, in: str!), with: atr)
+    //                }
+    //        }
+            
+            
+            if (str?.contains("+"))! {
+                
+                let r1 = str!.range(of: "+")!
+                let atr = self.customizeColor(string: "+", color: UIColor.systemOrange)
+                aString.replaceCharacters(in: NSRange(r1, in: str!), with: atr)
+            }
+            if (str?.contains("-"))! {
+                
+                let r1 = str!.range(of: "-")!
+                let atr = self.customizeColor(string: "-", color: UIColor.systemOrange)
+                aString.replaceCharacters(in: NSRange(r1, in: str!), with: atr)
+            }
+            if (str?.contains("*"))! {
+                
+                let r1 = str!.range(of: "*")!
+                let atr = self.customizeColor(string: "*", color: UIColor.systemOrange)
+                aString.replaceCharacters(in: NSRange(r1, in: str!), with: atr)
+            }
+            if (str?.contains("/"))! {
+                
+                let r1 = str!.range(of: "/")!
+                let atr = self.customizeColor(string: "/", color: UIColor.systemOrange)
+                aString.replaceCharacters(in: NSRange(r1, in: str!), with: atr)
+            }
+            if (str?.contains("%"))! {
+                
+                let r1 = str!.range(of: "%")!
+                let atr = self.customizeColor(string: "%", color: UIColor.systemOrange)
+                aString.replaceCharacters(in: NSRange(r1, in: str!), with: atr)
+            }
+            
+            if (str?.contains("of"))! {
+                
+                let r1 = str!.range(of: "of")!
+                let atr = self.customizeColor(string: "of", color: UIColor.systemPurple)
+                aString.replaceCharacters(in: NSRange(r1, in: str!), with: atr)
+            }
+            
+            //l1AttrString.append(self.customizeColor(string: "!", color: UIColor.red))
+            textView.attributedText = aString
     }
     
-    func lineCounter(_ textView: UITextView) -> Int {
-        let layoutManager:NSLayoutManager = textView.layoutManager
-        let numberOfGlyphs = layoutManager.numberOfGlyphs
-        print(numberOfGlyphs)
-        var numberOfLines = 0
-        var index = 0
-        var lineRange:NSRange = NSRange()
+    func  customizeColor(string: String, color: UIColor) -> NSMutableAttributedString {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 18),
+            .foregroundColor: color,
+        ]
         
-        while (index < numberOfGlyphs) {
-            print(layoutManager.lineFragmentRect(forGlyphAt: index, effectiveRange: &lineRange))
-            index = NSMaxRange(lineRange);
-            numberOfLines = numberOfLines + 1
-        }
-        return numberOfLines
+        return NSMutableAttributedString(string: string, attributes: attributes)
     }
-    
 }
 
-//class CalcTextView: UITextView {
-//
-//    override func deleteBackward() {
-//        super.deleteBackward()
-//
-//    }
-//}
+extension String {
+    func math() -> String {
+        let expr = Parser.parse(string: self)
+        guard let exprValue = expr?.evaluate() else { return "" }
+        return NSDecimalNumber(decimal: exprValue).stringValue
+    }
+}
+
+extension Array where Element == Substring {
+    mutating func transformOmittingEmptySubsequences (_ character: Substring) -> [Substring] {
+        for i in 0..<self.count {
+            if self[i] == "" {
+                self[i] = character
+            }
+        }
+        return self
+    }
+}
